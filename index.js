e648f556b3f268d21c596f1feb6edd822ee5573f
Thomas Neumann <thsneumann@gmail.com>
2021-01-17 18:19:50 +0100

    add loading spinner

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
 const byDistrict = (district) => (street) =>
   street.districts.includes(district);
 
-const App = new Vue({
-  el: "#app",
-  data: {
-    streets: STREETS_OF_BERLIN,
-    selectedStreet: null,
-    selectedDistrict: "all",
-  },
-  computed: {
-    districts() {
-      return new Set(
-        this.streets
-          .map((s) => s.districts)
-          .flat()
-          .sort()
-      );
+window.addEventListener("load", () => {
+  document.getElementById("loading-spinner").style.opacity = 0;
+
+  const App = new Vue({
+    el: "#app",
+    data: {
+      streets: STREETS_OF_BERLIN,
+      selectedStreet: null,
+      selectedDistrict: "all",
     },
-  },
-  methods: {
-    selectRandomStreet() {
-      if (this.selectedDistrict === "all") {
-        const i = Math.floor(Math.random() * this.streets.length);
-        this.selectedStreet = this.streets[i];
-      } else {
-        const streetsInDistrict = this.streets.filter(
-          byDistrict(this.selectedDistrict)
+    computed: {
+      districts() {
+        return new Set(
+          this.streets
+            .map((s) => s.districts)
+            .flat()
+            .sort()
         );
-        const i = Math.floor(Math.random() * streetsInDistrict.length);
-        const streetIndex = this.streets.findIndex(
-          (street) =>
-            street.google_maps_link === streetsInDistrict[i].google_maps_link
-        );
-        this.selectedStreet = this.streets[streetIndex];
-      }
-      Vue.nextTick(() =>
-        this.$refs.card.scrollIntoView({ behavior: "smooth" })
-      );
+      },
     },
-  },
+    methods: {
+      selectRandomStreet() {
+        if (this.selectedDistrict === "all") {
+          const i = Math.floor(Math.random() * this.streets.length);
+          this.selectedStreet = this.streets[i];
+        } else {
+          const streetsInDistrict = this.streets.filter(
+            byDistrict(this.selectedDistrict)
+          );
+          const i = Math.floor(Math.random() * streetsInDistrict.length);
+          const streetIndex = this.streets.findIndex(
+            (street) =>
+              street.google_maps_link === streetsInDistrict[i].google_maps_link
+          );
+          this.selectedStreet = this.streets[streetIndex];
+        }
+        Vue.nextTick(() =>
+          this.$refs.card.scrollIntoView({ behavior: "smooth" })
+        );
+      },
+    },
+  });
 });
